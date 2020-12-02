27c3e5a9709b22a84cdd662910dc1c5a30863f0f
EJ132 <ejg132@gmail.com>
2020-12-02 11:00:35 -0800

    compile errors

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 import evolveLogo from './evolveLogo.png'
-import {useEffect} from 'react'
 
 
 function App() {
   return (
     <div className="App"> 
       <div style={{background: "rgba(0,0,0,0.5)"}}>
-        <img style={{objectFit: "contain", height: "20%", width: "50%", paddingLeft: "10%", paddingRight: "10%"}} src={evolveLogo} />
+        <img alt="evovle gifts" style={{objectFit: "contain", height: "20%", width: "50%", paddingLeft: "10%", paddingRight: "10%"}} src={evolveLogo} />
         <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeu70PBUTSyZ8B-q2hNCVXoB5OAIaRpe9X2MNMRpMplU_EA6w/viewform?embedded=true" width="640" height="700" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
         {/* <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeu70PBUTSyZ8B-q2hNCVXoB5OAIaRpe9X2MNMRpMplU_EA6w/viewform?embedded=true" style={{height: "700px !important"}} width="640" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe> */}
       </div>
