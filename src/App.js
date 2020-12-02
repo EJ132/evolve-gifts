1b01e1b6b9d76c091599c555eaa65035c493004b
EJ132 <ejg132@gmail.com>
2020-12-02 10:36:01 -0800

    google form updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import evolveLogo from './evolveLogo.png'
+import {useEffect} from 'react'
+
 
 function App() {
+
+  useEffect(() => {
+    console.log(document.getElementsByName("body"));
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App"> 
+      <div style={{background: "rgba(0,0,0,0.5)"}}>
+        <img style={{height: "20%", width: "50%", paddingLeft: "10%", paddingRight: "10%"}} src={evolveLogo} />
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeu70PBUTSyZ8B-q2hNCVXoB5OAIaRpe9X2MNMRpMplU_EA6w/viewform?embedded=true" width="640" height="700" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+        {/* <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeu70PBUTSyZ8B-q2hNCVXoB5OAIaRpe9X2MNMRpMplU_EA6w/viewform?embedded=true" style={{height: "700px !important"}} width="640" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe> */}
+      </div>
+    </div> 
   );
 }
 
