19933326af92a2bde9ec377c8d58b58632f7ac7b
EJ132 <ejg132@gmail.com>
2020-12-02 10:54:35 -0800

    updating site

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import evolveLogo from './evolveLogo.png'
 import {useEffect} from 'react'
 
 
 function App() {
-
-  useEffect(() => {
-    console.log(document.getElementsByName("body"));
-  });
-
   return (
     <div className="App"> 
       <div style={{background: "rgba(0,0,0,0.5)"}}>
